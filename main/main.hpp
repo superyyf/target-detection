9d146ff7633883984e4f5e4f78da7b7ecf8e5fb6
superyyf <1420328500@qq.com>
2020-08-17 09:23:03 +0800

    debug

--- main/main.hpp
+++ main/main.hpp
@@ -105,11 +105,11 @@ int UART0_Recv(int fd, char *rcv_buf,int data_len)
         FD_ZERO(&fs_read);    
         FD_SET(fd,&fs_read);    
            
-        //time.tv_sec = 0;    
-        //time.tv_usec = 12000;    
+        time.tv_sec = 0;    
+        time.tv_usec = 12000;    
            
         //使用select实现串口的多路通信    
-        fs_sel = select(fd+1,&fs_read,NULL,NULL,NULL);    
+        fs_sel = select(fd+1,&fs_read,NULL,NULL,&time);    
         if(fs_sel)    
         {    
             len = read(fd,rcv_buf,data_len);    
@@ -339,7 +339,7 @@ void set_system_time()
 		int len = UART0_Recv(fd, rcv_buf,sizeof(ReceiveInfo));    
         	if(len < 0){    
 	
-                    	printf("cannot receive data\n");    
+                    	printf("Cannot Receive Data!\n");    
                 }    
 		else{
 			rcv_info = reinterpret_cast<ReceiveInfo *>(rcv_buf);
